3fbc6a10b23893b94603bdff96ff9f9f6a947d67
Vishal Bachu <vishal@greenfulfilment.co.uk>
2025-08-20 00:00:34 +0530

    API test - 1

---
+++ api/hello.js
@@ -0,0 +1,12 @@
+// This is a Node.js serverless function.
+// It will be deployed at the URL: /api/hello
+
+export default function handler(request, response) {
+  // The function receives a request and sends a response.
+  
+  // Set the status to 200 (OK) and send a JSON object back.
+  response.status(200).json({
+    message: "Hello from the Vercel backend! 👋",
+    timestamp: new Date().toISOString(),
+  });
+}
